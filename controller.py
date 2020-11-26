3d4b34f03c855f0d82ca58c49d97725a9668a612
colorpen2010 <melnik.olga.22.11.1981@gmail.com>
2020-11-26 08:08:55 +0200

    i like programi

---
+++ controller.py
@@ -0,0 +1,33 @@
+import pygame, time, random, settings, model
+
+
+def obrabotca_sobitiy():
+    # обработка событий
+    coffin_dance = pygame.event.get()
+
+    for e in coffin_dance:
+        # if e.type == pygame.MOUSEMOTION:
+        #     в право
+            # sdvigx = e.pos[0] - rect.centerx
+            # if sdvigx >= 1:
+            #     rect.x += sdvigx
+            #     e1 = rect.colliderect(w)
+            #     if e1 == 1:
+            #         rect.right = w.left
+            # в лево
+            # if sdvigx <= -1:
+            #     rect.x -= 20
+            #     e1 = rect.colliderect(w)
+            #     if e1 == 1:
+            #         rect.left = w.right
+
+        if e.type == pygame.KEYDOWN:
+
+            if e.key == pygame.K_RIGHT:
+               model.platformirovanie()
+
+            # if e.key == pygame.K_LEFT:
+                # rect.x -= 20
+                # e1 = rect.colliderect(w)
+                # if e1 == 1:
+                #     rect.left = w.right
